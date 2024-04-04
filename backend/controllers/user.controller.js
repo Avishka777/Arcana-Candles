11cac45516be7211776ac4edb455429617ee46ce
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-04 12:26:37 +0530

    Add Update User API Route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,52 @@
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import User from '../models/user.model.js';
+
+
+//Test API is Working
 export const test = (req, res) => {
-    res.json({ message: 'API is working!' });
-  };
+  res.json({ message: 'API is working!' });
+};
+
+
+//Update User Details
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this user'));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, 'Password must be at least 6 characters'));
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.userName) {
+    if (!req.body.userName.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, 'User Name Can Only Contain Letters And Numbers')
+      );
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        {
+          $set: {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName,
+            email: req.body.email,
+            address: req.body.address,
+            mobile: req.body.mobile,
+            profilePicture: req.body.profilePicture,
+            password: req.body.password,
+          },
+        },
+        { new: true }
+      );
+      const { password, ...rest } = updatedUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  }
+};
