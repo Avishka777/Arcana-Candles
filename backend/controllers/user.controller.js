b757c7c14d1c80f5ede73c4ab5e51e871ef02ecf
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-04 19:24:28 +0530

    Add Delete User API Route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -50,3 +50,17 @@ export const updateUser = async (req, res, next) => {
       next(error);
     }
 };
+
+
+//Delete User Account
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You Are Not Allowed To Delete This User'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User Has Been Deleted');
+  } catch (error) {
+    next(error);
+  }
+};
