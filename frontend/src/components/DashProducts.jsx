6cf14bb9ac92143e2e6bead9e57e9b6242365cf6
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-05 07:05:34 +0530

    Show Products Inside Dashboard

--- frontend/src/components/DashProducts.jsx
+++ frontend/src/components/DashProducts.jsx
@@ -1,7 +1,90 @@
-import React from 'react'
+import { Table } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function DashProducts() {
+
+  const { currentUser } = useSelector((state) => state.user);
+  const [userProducts, setUserProducts] = useState([]);
+  console.log(userProducts);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch(`/api/product/getproducts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserProducts(data.products);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchProducts();
+    }
+  }, [currentUser._id]);
+
   return (
-    <div>DashProducts</div>
-  )
+    
+    <div className='w-full p-5 table-auto overflow-x-scroll md:mx-auto scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      {currentUser.isAdmin && userProducts.length > 0 ? (
+        <>
+          <Table hoverable className='shadow-md'>
+            <Table.Head>
+              <Table.HeadCell>Date updated</Table.HeadCell>
+              <Table.HeadCell>Product Image</Table.HeadCell>
+              <Table.HeadCell>Product Name</Table.HeadCell>
+              <Table.HeadCell>Price</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userProducts.map((product) => (
+              <Table.Body className='divide-y'>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                  
+                  <Table.Cell>{new Date(product.updatedAt).toLocaleDateString()}</Table.Cell>
+                  
+                  <Table.Cell>
+                    <Link to={`/product/${product.slug}`}>
+                      <img src={product.image}  alt={product.title} className='w-20 h-10 object-cover bg-gray-500' />
+                    </Link>
+                  </Table.Cell>
+                  
+                  <Table.Cell>
+                    <Link className='font-medium text-gray-900 dark:text-white'  to={`/product/${product.slug}`} >
+                      {product.title}
+                    </Link>
+                  </Table.Cell>
+                  
+                  <Table.Cell>{product.price}</Table.Cell>
+
+                  <Table.Cell>{product.category}</Table.Cell>
+                  
+                  <Table.Cell>
+                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>
+                      Delete
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className='text-teal-500 hover:underline'
+                      to={`/update-product/${product._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>You Have No Products Yet.</p>
+      )}
+    </div>
+  );
 }
