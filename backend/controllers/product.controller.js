efb3cab2bba9a91387b17c3a8ee18794d841cdb8
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-05 06:31:58 +0530

    Add Get Prodcts API Route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -27,3 +27,50 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+
+//Get Product Details
+export const getproducts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const products = await Product.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.productId && { _id: req.query.productId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalProducts = await Product.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthProducts = await Product.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      products,
+      totalProducts,
+      lastMonthProducts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/product.controller.js';
+import { create, getproducts } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
-
+router.get('/getproducts', getproducts)
 
 export default router;
