e79ba5199656970843bbe1cb4ba0b756a74df8cb
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-05 08:28:51 +0530

    Add Delete User Function To The Admin User

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -54,7 +54,7 @@ export const updateUser = async (req, res, next) => {
 
 //Delete User Account
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You Are Not Allowed To Delete This User'));
   }
   try {
