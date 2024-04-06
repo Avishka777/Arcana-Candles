62f01ae7ca9f5c4047b1acfef392b63de171fe94
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-06 18:35:28 +0530

    Change Some Styles On Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import ProductCard from '../components/ProductCard';
 import test from '../assets/test.jpg';
 import i from '../assets/3.jpg';
+import promo1 from '../assets/promo1.jpg';
+import promo2 from '../assets/promo2.jpg';
 
 export default function Home() {
   const [products, setProducts] = useState([]);
@@ -18,26 +20,26 @@ export default function Home() {
   
   return (
     <div>
-      <div className='flex flex-col gap-6 py-20 px-20 max-w mx-auto'>
+      <div className='flex flex-col gap-6 pt-20 px-20 max-w mx-auto'>
         <div className='text-center'>
           <h1 className='text-3xl font-bold lg:text-6xl font-serif mb-3'>Welcome to Arcana Candles</h1>
           <h1 className='text-xl font-bold lg:text-4xl font-serif text-gray-500'>Where Your Dreams Come True...</h1>
         </div>       
       </div>
       
-      <div className='px-20 mx-auto p-3 flex flex-col gap-8 py-7'>
-        <div className='flex mx-auto px-20'>
-          <img src={i} className="mr-3 h-20 w-1/2 sm:h-auto rounded-lg" />
-          <img src={i} className="mr-3 h-20 w-1/2 sm:h-auto rounded-lg" />
+      <div className='mx-10 sm:mx-40  p-3 flex flex-col gap-8 py-7'>
+        <div className='flex sm:mx-auto mx-auto w-full'>
+          <img src={promo1} className="p-1 h-30 w-1/2 sm:h-auto rounded-lg" />
+          <img src={promo2} className="p-1 h-30 w-1/2 sm:h-auto rounded-lg" />
         </div>
       </div>
 
-      <div className='px-20 mx-auto p-3 flex flex-col gap-8 py-7'>
+      <div className='sm:mx-20 p-3 gap-8 py-7'>
         {products && products.length > 0 && (
           <div className='flex flex-col gap-6'>
-            <h2 className='text-3xl font-semibold text-center'>Our Best Sellers...</h2>
-            <hr class="border-b-1 border-gray-500"/>
-            <div className='flex flex-wrap gap-4 mx-auto'>
+            <h2 className='text-3xl font-semibold sm:ml-20 mx-10'>Our Best Sellers...</h2>
+            <hr class="border-b-1 border-gray-500 sm:mx-20 mx-10"/>
+            <div className='flex flex-wrap gap-4 mx-10 sm:mx-auto '>
               {products.map((product) => (
                 <ProductCard key={product._id} product={product} />
               ))}
@@ -52,31 +54,31 @@ export default function Home() {
         )}
       </div>
 
-      <div className='px-20 mx-auto p-3 flex flex-col gap-8 py-7'>
-        <h2 className='text-3xl font-semibold text-center'>Find Your Vibe...</h2>
-        <hr class="border-b-1 border-gray-500"/>
-        <div className='flex mx-auto'>
-          <img src={test} className="mr-3 h-20 w-72 sm:h-auto rounded-lg" />
+      <div className='sm:mx-20 p-3 flex flex-col gap-8 py-7'>
+        <h2 className='text-3xl font-semibold sm:mx-20 mx-10'>Find Your Vibe...</h2>
+        <hr class="border-b-1 border-gray-500 sm:mx-20 mx-10"/>
+        <div className='flex sm:mx-20 mx-10'>
+          <img src={test} className="px-1 sm:px-1 h-36 w-72 sm:h-auto rounded-lg" />
           <div>
-            <img src={test} className="mr-3 h-20 w-72 sm:h-56 rounded-lg" />
-            <img src={test} className="mr-3 mt-3 h-20 w-72 sm:h-56 rounded-lg" />
+            <img src={test} className="px-1 sm:px-1 h-16 w-72 sm:h-56 rounded-lg" />
+            <img src={test} className="px-1 sm:px-1 mt-4 h-16 w-72 sm:h-56 rounded-lg" />
           </div>
-          <img src={test} className="mr-3 h-20 w-72 sm:h-auto rounded-lg" />
+          <img src={test} className="px-1 sm:px-1 h-36 w-72 sm:h-auto rounded-lg" />
           <div>
-            <img src={test} className="mr-3 h-36 w-72 sm:h-56 rounded-lg" />
-            <img src={test} className="mr-3 mt-3 h-20 w-72 sm:h-56 rounded-lg" />
+            <img src={test} className="px-1 sm:px-1 h-16 w-72 sm:h-56 rounded-lg" />
+            <img src={test} className="px-1 sm:px-1 mt-4 h-16 w-72 sm:h-56 rounded-lg" />
           </div>
-          <img src={test} className="mr-3 h-20 w-72 sm:h-auto rounded-lg" />
+          <img src={test} className="px-1 sm:px-1 h-36 w-72 sm:h-auto rounded-lg" />
         </div>
       </div>
 
-      <div className='px-20 mx-auto p-3 flex flex-col gap-8 py-7'>
-        <h2 className='text-3xl font-semibold text-center'>See Our Customer Feedbacks...</h2>
-        <hr class="border-b-1 border-gray-500"/>
-        <div className='flex mx-auto'>
-          <img src={i} className="mr-3 h-20 w-1/3 sm:h-auto rounded-lg p-10" />
-          <img src={i} className="mr-3 h-20 w-1/3 sm:h-auto rounded-lg p-10" />
-          <img src={i} className="mr-3 h-20 w-1/3 sm:h-auto rounded-lg p-10" />
+      <div className='sm:mx-20 p-3 flex flex-col gap-8 py-7'>
+        <h2 className='text-3xl font-semibold sm:mx-20 mx-10'>Customer Feedbacks...</h2>
+        <hr class="border-b-1 border-gray-500 sm:mx-20 mx-10"/>
+        <div className='flex sm:mx-20 mx-10'>
+          <img src={i} className="mr-3 h-20 w-1/3 sm:h-auto rounded-lg p-1" />
+          <img src={i} className="mr-3 h-20 w-1/3 sm:h-auto rounded-lg p-1" />
+          <img src={i} className="mr-3 h-20 w-1/3 sm:h-auto rounded-lg p-1" />
         </div>
       </div>
     </div>
