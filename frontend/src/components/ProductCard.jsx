4f551a95c1c3b8ab48697a8f382faabbc5111273
Avishka777 <a.rathnakumara777@gmail.com>
2024-04-06 14:33:28 +0530

    Change Some Styles In Product Card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,24 +1,44 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ProductCard({ product }) {
+
+  const openWhatsApp = (phoneNumber, message) => {
+    const encodedMessage = encodeURIComponent(message);
+    window.open(`https://wa.me/${phoneNumber}?text=${encodedMessage}`, '_blank');
+  };
+  
   return (
-    <div className='group relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all'>
+
+    <div className='group relative w-full border border-teal-500 hover:border-2 h-[380px] overflow-hidden rounded-lg sm:w-[230px] transition-all'>
+      
       <Link to={`/product/${product.slug}`}>
         <img
           src={product.image}
-          alt='product cover'
-          className='h-[260px] w-full  object-cover group-hover:h-[200px] transition-all duration-300 z-20'
+          alt='Product Image'
+          className='h-[180px] w-full  object-cover group-hover:h-[200px] transition-all duration-300 z-20'
         />
       </Link>
+
       <div className='p-3 flex flex-col gap-2'>
-        <p className='text-lg font-semibold line-clamp-2'>{product.title}</p>
-        <span className='italic text-sm'>{product.category}</span>
+        
+        <p className='italic text-xl font-semibold line-clamp-2 '>{product.title}</p>
+        <p className='italic text-base text-gray-500'>{product.category}</p>
+        <p className='italic text-sm'>{product.price}</p>
+        
+        <button 
+          className='z-10 group-hover:bottom-14 absolute bottom-[50px] left-0 right-0 border border-emerald-600 text-emerald-600 hover:bg-emerald-600 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
+          onClick={() => openWhatsApp('+94760733387', 'Hello, I am interested in your products.')}>
+          Order via WhatsApp.
+        </button>
+
         <Link
           to={`/product/${product.slug}`}
-          className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
+          className='z-10 group-hover:bottom-1 absolute bottom-[00px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
         >
-          More Details
+          More Details...
         </Link>
+
       </div>
     </div>
   );
